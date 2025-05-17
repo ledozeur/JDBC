3b4940c8a1218619c98a9b5cd04f8f8ac1a4e48a
ledozeur <git@github.com>
2025-05-18 02:52:18 +0300

    Work

--- jdbs/src/main/java/ru/netology/jdbs/JdbsApplication.java
+++ jdbs/src/main/java/ru/netology/jdbs/JdbsApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "jdbs")
-
-@EntityScan(basePackages = "jdbs")
 public class JdbsApplication {
 
 	public static void main(String[] args) {
--- jdbs/src/main/resources/db/changelog/migrations/data.sql
+++ jdbs/src/main/resources/db/changelog/migrations/data.sql
@@ -1,5 +1,5 @@
 -- liquibase formatted sql
--- changeset vladimir:1
+
 SELECT product_name
 FROM netology.ORDERS
 JOIN netology.CUSTOMERS ON ORDERS.customer_id = CUSTOMERS.id
